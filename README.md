47ebb7702e489b21ae4bf178c043db2ad0857f7b
vasa <vasa@towardsblockchain.com>
2019-04-09 00:59:23 +0530

    version 1.0.3

--- README.md
+++ README.md
@@ -8,80 +8,61 @@ import rootReducer from '../reducers'
 import apiMiddleWare from 'redux-cross-fetch'
 
 const store = createStore(rootReducer, applyMiddleware(apiMiddleWare))
-window.store = store;
 
 export default store
 ```
 
-*Create actions with `type` including the string `API_REQUEST` to fire the middleware.*
 
 The middleware takes following params:
 
 
-        @{url}: [REQUIRED] the request URL
+```
+        @param {url}: [REQUIRED] the request URL
 
-        @{method}: [REQUIRED] HTTP method
+        @param {method}: [REQUIRED] HTTP method
 
-        @{headers}: [OPTIONAL] headers sent with the HTTP request
+        @param {headers}: [OPTIONAL] headers sent with the HTTP request
         
-        @{body}: [OPTIONAL] body sent with the HTTP request
-
-        @{success}: [REQUIRED] action creator for successful response 
+        @param {body}: [OPTIONAL] body sent with the HTTP request
+        
+        @param {success}: [REQUIRED] action creator for successful response 
         Expects a payload for Server Response 
 
-        @{failed}: [REQUIRED] action creator for failed response
+        @param {failure}: [REQUIRED] action creator for failed response
         Expects a payload for Server Error
+```
 
-        @{start}: [REQUIRED] action creator for starting API Request
-        Expects a payload for action
-
-        @{stop}: [REQUIRED] action creator for ending API Request
-        Expects a payload for action
-        
 
 ## actions.js
 
+
+*Create actions with `type` including the string `API_REQUEST` to fire the middleware.*
+
+
 ```
-export const getGitHubProfile = username => {
-    return({
-        type: "[GITHUB] API_REQUEST",
-        payload: {
-            url: "https://api.github.com/users/cluster-labs",
-            method: "GET",
-            success: responseSuccess,
-            failed: responseFailed,
-            start: startFetching,
-            stop: stopFetching
-        }
-    })
-}
-
-export const responseSuccess = response => {
-    return{
-        type: types.RESPONSE_SUCCESS,
-        payload: response
+export const testAction = () => ({
+    type: "API_REQUEST",
+    payload: {
+        request: {
+            url: "https://api.github.com/users/vasa-develop",
+            method: "GET"
+        },
+        success: apiSuccess,
+        failure: apiFailure
     }
-}
+})
 
-export const responseFailed = error => {
-    console.log( types.RESPONSE_FAILED+"  "+error)
-    return{
-        type: types.RESPONSE_FAILED,
-        payload: error
+export const apiSuccess = (response) => ({
+    type: "API_SUCCESS",
+    payload: {
+        response: response
     }
-}
+})
 
-export const startFetching = action => {
-    return{
-        type: types.FETCH_START,
-        payload: action
+export const apiFailure = (error) => ({
+    type: "API_FAILURE",
+    payload: {
+        error: error
     }
-}
-
-export const stopFetching = action => {
-    return{
-        type: types.FETCH_STOP,
-        payload: action
-    }
-}
+})
 ```
--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 import fetch from 'cross-fetch'
 
 export default ({dispatch, getState}) => next => action => {
+
     const API_TAG = "[API MIDDLEWARE]";
 
     console.log(`-------${API_TAG}-------`)
-    console.log(action.type)
+    action.type ? console.log(action.type) : console.log(action) 
 
-    if(action.type.includes("API_REQUEST")){
+    if(action.type && action.type.includes("API_REQUEST")){
         
         /*
-        @param url: [REQUIRED] the request URL
 
-        @param method: [REQUIRED] HTTP method
+        @param {url}: [REQUIRED] the request URL
 
-        @param headers: [OPTIONAL] headers sent with the HTTP request
+        @param {method}: [REQUIRED] HTTP method
+
+        @param {headers}: [OPTIONAL] headers sent with the HTTP request
         
-        @param body: [OPTIONAL] body sent with the HTTP request
-
-        @param success: [REQUIRED] action creator for successful response 
+        @param {body}: [OPTIONAL] body sent with the HTTP request
+        
+        @param {success}: [REQUIRED] action creator for successful response 
         Expects a payload for Server Response 
 
-        @param failed: [REQUIRED] action creator for failed response
+        @param {failure}: [REQUIRED] action creator for failed response
         Expects a payload for Server Error
 
-        @param start: [REQUIRED] action creator for starting API Request
-        Expects a payload for action
-
-        @param stop: [REQUIRED] action creator for ending API Request
-        Expects a payload for action
         */
 
-        const { url, method, headers = {}, body = {}, success, failed, start, stop } = action.payload;
         
+        const { request, headers = {}, body = {}, success, failure } = action.payload;
+        const { url, method } = request
+
         const throwError = error => {
             console.error(error)
             next(action)
         }
-
         const isValidUrl = string => {
             try {
               new URL(string);
@@ -45,18 +43,12 @@ export default ({dispatch, getState}) => next => action => {
               return false;  
             }
           }
-
-        typeof(stop) !== "function"  ? throwError(`${API_TAG} success needs to a function`) : console.log()
-        typeof(stop) !== "function"  ? throwError(`${API_TAG} falied needs to a function`) : console.log()
-        typeof(stop) !== "function"  ? throwError(`${API_TAG} start needs to a function`) : console.log()
-        typeof(stop) !== "function"  ? throwError(`${API_TAG} stop needs to a function`) : console.log()
+        typeof(success) !== "function"  ? throwError(`${API_TAG} success needs to a function`) : console.log()
+        typeof(failure) !== "function"  ? throwError(`${API_TAG} falied needs to a function`) : console.log()
         typeof(url) !== "string" ? throwError(`${API_TAG} url missing`) : console.log()
         typeof(method) !== "string" ? throwError(`${API_TAG} HTTP method missing`) : console.log()
         isValidUrl(url) ? console.log() : throwError(`${API_TAG} isValid URL provided`)
         
-        //Dispatching action to notify the application that the api request has started
-        dispatch(start(action));
-
         let params = {
             method: method,
             headers: headers
@@ -64,22 +56,15 @@ export default ({dispatch, getState}) => next => action => {
         if(method != ("GET" || "HEAD")){
             params.body = body
         }
-
         fetch(url,params)
         .then(res => ( res.ok ? res.json() : new Error('Something went wrong') ) )
         .then(data => {
-            //Dispatching action to notify the application that the api request has ended
-            dispatch(stop(action))
-            
             //Dispatching action to pass the fetched data
             next(success(data))
         })
         .catch(error => {
-            //Dispatching action to notify the application that the api request has ended
-            dispatch(stop(action))
-            
             //Dispatching action to pass the fetched data
-            next(failed(error))
+            next(failure(error))
         })
     }
     else next(action) //Dispatching action normally, if the action is not of type API_REQUEST
